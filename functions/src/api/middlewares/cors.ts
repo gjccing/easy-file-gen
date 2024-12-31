d558d4e20a6c40d11c161b43e360d0ff9b13e536
Harvey <j2160204@gmail.com>
2024-12-31 16:59:43 +0800

    simplify the cors middleware

--- functions/src/api/middlewares/cors.ts
+++ functions/src/api/middlewares/cors.ts
@@ -1,21 +1,10 @@
-import type { RequestHandler } from "express";
 import type { Request } from "express";
 import cors from "cors";
 import * as admin from "firebase-admin";
 
 const db = admin.firestore();
 
-const resetCorsFromFirebase: RequestHandler = (req, res, next) => {
-  res.removeHeader("Access-Control-Allow-Origin");
-  res.removeHeader("Access-Control-Allow-Methods");
-  res.removeHeader("Access-Control-Allow-Credentials");
-  res.removeHeader("Access-Control-Allow-Headers");
-  res.removeHeader("Access-Control-Expose-Headers");
-  res.removeHeader("Access-Control-Max-Age");
-  next();
-};
-
-const _cors = cors(async (req: Request, callback) => {
+export default cors(async (req: Request, callback) => {
   try {
     const doc = await db
       .collection("settings")
@@ -28,5 +17,3 @@ const _cors = cors(async (req: Request, callback) => {
     callback(e);
   }
 });
-
-export default [resetCorsFromFirebase, _cors];
